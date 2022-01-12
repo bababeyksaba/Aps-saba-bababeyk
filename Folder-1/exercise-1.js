4ef05f37e38c74eba23b793fd38a7151754db458
saba bababeyk <90498006+bababeyksaba@users.noreply.github.com>
2022-01-12 20:53:58 +0330

    commit-1

---
+++ Folder-1/exercise-1.js
@@ -0,0 +1,14 @@
+const person = {
+    name : "Saba Bababeyk",
+    weight : "60kg",
+    height : "170cm",
+    status : "student",
+    favColor :["blue","black"],
+    age : 20,
+
+}
+console.log(person.name)
+console.log(person["weight"])
+person["name"] = "Asghar";
+console.log(person.name)
+console.log(this.person)
